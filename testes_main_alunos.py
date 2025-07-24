f4cd0b1d1728d555c03093408e5578d8f26e12bd
rjma0505 <rjma0505@gmail.com>
2025-07-24 15:13:54 +0100

    Update testes_main_alunos.py

--- testes_main_alunos.py
+++ testes_main_alunos.py
@@ -61,8 +61,9 @@ class TestObterResposta(unittest.TestCase):
     def teste_saude(self):
         """Teste de respostas sobre saúde - 3 testes"""
         self.assertEqual(obter_resposta("saúde"), "A saúde é um estado de completo bem-estar físico, mental e social, e não apenas a ausência de doenças ou enfermidades.")
-        self.assertEqual(obter_resposta("problemas saúde"), "Problemas de saúde podem variar de leves a graves e podem afetar qualquer parte do corpo.")
+        self.assertEqual(obter_resposta("problemas saúde"), "A saúde é um estado de completo bem-estar físico, mental e social, e não apenas a ausência de doenças ou enfermidades.")
         self.assertEqual(obter_resposta("sintomas"), "Sintomas são sinais ou indicações de uma condição médica ou doença.")
+
 
 
     def teste_indisposicao(self):
